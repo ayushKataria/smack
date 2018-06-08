517746b797057122909a196143459a963aea4604
ayushKataria <akat21121997Agmail.com>
2018-06-08 23:29:55 +0530

    lesson-14

--- app/src/main/java/com/example/akat2/smack/controller/MainActivity.kt
+++ app/src/main/java/com/example/akat2/smack/controller/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.akat2.smack.controller
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.example.akat2.smack.R
+import com.example.akat2.smack.services.AuthService
+import com.example.akat2.smack.services.UserDataService
+import com.example.akat2.smack.utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+            override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
@@ -23,6 +32,24 @@ class MainActivity : AppCompatActivity() {
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver, IntentFilter(BROADCAST_USER_DATA_CHANGE))
+    }
+
+    private val userDataChangeReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+
+            if(AuthService.isLogggedIn){
+                userNameNavHeader.text = UserDataService.name
+                userEmailNavHeader.text = UserDataService.email
+
+                val resourceId = resources.getIdentifier(UserDataService.avatarName, "drawable", packageName)
+                userImageNavHeader.setImageResource(resourceId)
+                userImageNavHeader.setBackgroundColor(UserDataService.getAvatarColor(UserDataService.avatarColor))
+
+                loginBtnNavHeader.text = "Logout"
+            }
+
+        }
     }
 
     override fun onBackPressed() {
@@ -34,8 +61,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked(view: View) {
-        val loginIntent = Intent(this, LoginActivity::class.java)
-        startActivity(loginIntent)
+        if(AuthService.isLogggedIn) {
+            //Log out
+            UserDataService.logout()
+            userNameNavHeader.text = "Please Login"
+            userEmailNavHeader.text = ""
+            userImageNavHeader.setImageResource(R.drawable.profiledefault)
+            userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
+            loginBtnNavHeader.text = "Login"
+        }else {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
     }
 
     fun addChannelClicked(view: View) {
--- app/src/main/java/com/example/akat2/smack/services/UserDataService.kt
+++ app/src/main/java/com/example/akat2/smack/services/UserDataService.kt
@@ -1,5 +1,8 @@
 package com.example.akat2.smack.services
 
+import android.graphics.Color
+import java.util.*
+
 /**
  * Created by Ayush Kataria on 08-06-2018.
  */
@@ -11,4 +14,37 @@ object UserDataService {
     var email = ""
     var name = ""
 
+    fun getAvatarColor(components: String) : Int {
+
+        val strippedColor = components
+                .replace("[", "")
+                .replace("]", "")
+                .replace(",", "")
+
+        var r = 0
+        var g = 0
+        var b = 0
+
+        val scanner = Scanner(strippedColor)
+        if(scanner.hasNext()) {
+            r = ((scanner.nextDouble())*255).toInt()
+            g = ((scanner.nextDouble())*255).toInt()
+            b = ((scanner.nextDouble())*255).toInt()
+        }
+
+        return Color.rgb(r, g, b)
+    }
+
+    fun logout() {
+        id = ""
+        avatarColor = ""
+        avatarName = ""
+        email = ""
+        name = ""
+        AuthService.authToken = ""
+        AuthService.userEmail = ""
+        AuthService.isLogggedIn = false
+    }
 }
+
+
